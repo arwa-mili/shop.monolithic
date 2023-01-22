e8287b158046d14d255a580cedd7fda2ff23623f
Arwa Mili <mili.arwa.ma@gmail.com>
2023-01-22 18:11:27 +0100

    change

--- src/main/java/arwa/ContainerResource.java
+++ src/main/java/arwa/ContainerResource.java
@@ -9,7 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class GreetingResource {
+public class ContainerResource {
 
    @GET
    @Produces(MediaType.TEXT_PLAIN)
--- src/test/java/arwa/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package arwa;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResource {
-    // Execute the same tests but in packaged mode.
-}
